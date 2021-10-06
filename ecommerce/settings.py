fce0c3a5f2f948823b4b02f95c14d5762e39a7c2
Feruzbek20020929 <feruznarzullayev547@gmail.com>
2021-10-06 13:24:21 +0500

    aaaa

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,7 +137,3 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
